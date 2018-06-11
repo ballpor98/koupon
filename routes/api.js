2748cacc65532e4bd1a136853345932c76237c3c
Chawin <ballpor98@gmail.com>
2018-06-11 18:22:39 +0700

    add random string

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
-var express = require('express');
+const uuidv4 = require('uuid/v4');
 var router = express.Router();
 
 var obj = require("../api/send.json");
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  let couponCode = uuidv4();
+  obj.fulfillmentMessages[0].text.text = String(couponCode);
   res.json(obj);
 });
 
