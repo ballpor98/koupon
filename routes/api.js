ab0db533fc678f7901147d2baea075c9888de4d8
Chawin <ballpor98@gmail.com>
2018-06-11 16:39:28 +0700

    remove req log

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var obj = require("../api/send.json");
 
 router.post('/', function(req, res, next) {
-  console.log(req);
+  //console.log(req);
   res.json(obj);
 });
 
