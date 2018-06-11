75bfde77c3dfb7d6acfc281b04e399d311a60e7f
Chawin <ballpor98@gmail.com>
2018-06-11 16:54:33 +0700

    add bodyparser to app.js

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
 var compression = require('compression');
+var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// helmet middleware
+app.use(helmet());
+// gzip compression middleware
+app.use(compression());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 //app.get('/', (req, res) => res.render('pages/index'))
 app.use('/', indexRouter);
 //app.post('/api', (req, res) => res.json(obj))
 app.use('/api', apiRouter);
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
 /*
