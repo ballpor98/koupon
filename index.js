5e02253cabe793dc9500fa62e0c7c78706f6a00e
Chawin <ballpor98@gmail.com>
2018-06-11 11:47:47 +0700

    api set path 2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var obj = require("./api/send.json");
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('/api',(req, res) => res.json('send'))
+  .post('/api',(req, res) => res.json(obj))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 /*
 {
