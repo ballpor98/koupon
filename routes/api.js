2bc08f57cd39f8d35f75a10dd573ba04bbf5e572
Chawin <ballpor98@gmail.com>
2018-06-12 15:26:02 +0700

    add coupon map

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,12 @@ var obj = require("../api/send.json");
 
 router.post('/', function(req, res, next) {
   //console.log(req.body.queryResult.parameters);
-  var para = req.body.queryResult.parameters;
-  console.log(para['shop-name']);
+  //var para = req.body.queryResult.parameters;
+  //console.log(req.body.queryResult.parameters['shop-name']);
   let couponCode = uuidv4();
-  obj.fulfillmentMessages[0].text.text[0] = String("coupon"+couponCode);
+  coupon[couponCode] = req.body.queryResult.parameters['shop-name'];
+  console.log(coupon[couponCode]);
+  obj.fulfillmentMessages[0].text.text[0] = String("couponCodeIs: "+couponCode);
   //+req.body.parameters['shop-name']
   res.json(obj);
 });
