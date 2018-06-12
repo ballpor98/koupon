67bc4a4502f7d485ec0b71cd024af3fff85769d0
Chawin <ballpor98@gmail.com>
2018-06-12 17:34:48 +0700

    try to gen qrcode

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,19 @@ router.post('/', function(req, res, next) {
   //parameters
   //req.body.queryResult.parameters['shop-name']
   let couponCode = uuidv4();
-  coupon[couponCode] = true;
+  QRCode.toDataURL(couponCode, function (err, url) {
+    coupon[couponCode] = url;
+})
   obj.fulfillmentMessages[0].text.text[0] = String("coupon Code: "+couponCode);
   //toDataURL(text, [options], [cb(error, url)])
-  //obj.fulfillmentMessages[1].image.imageUri = "https://kouponcoin.herokuapp.com/api/"+couponCode;
+  obj.fulfillmentMessages[1].image.imageUri = "https://kouponcoin.herokuapp.com/api/"+couponCode;
   res.json(obj);
-  QRCode.toDataURL(couponCode, function (err, url) {
-  console.log(url)
-})
+});
 
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("ID: "id);
+  res.render(coupon[id]);
 });
 /*
 var opts = {
@@ -35,17 +39,5 @@ QRCode.toDataURL('text', opts, function (err, url) {
   img.src = url
 })
 */
-/*
-router.get('/:id', function(req, res) {
-    var id = req.params.id
-    var qrcode = req.qrcode();
-    qrcode.setDimension(240,240);
-    qrcode.setCharset('UTF-8');
-    //qrcode.setCharset('UTF-8');
-    qrcode.setCorrectionLevel('L',0);
-    qrcode.setData(id);
-    var image = qrcode.getImage();
-  res.render('pages/image', { title: 'Express',img:image });
-});
-*/
+
 module.exports = router;
