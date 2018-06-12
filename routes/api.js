c569ce04208df5dfab54d83bd250c494deec201d
Chawin <ballpor98@gmail.com>
2018-06-12 10:29:42 +0700

    add parameters rec

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,10 @@ var router = express.Router();
 var obj = require("../api/send.json");
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
+  console.log(req.body.parameters['shop-name']);
   let couponCode = uuidv4();
-  obj.fulfillmentMessages[0].text.text[0] = String(couponCode);
+  obj.fulfillmentMessages[0].text.text[0] = String("coupon"+couponCode);
+  //+req.body.parameters['shop-name']
   res.json(obj);
 });
 
