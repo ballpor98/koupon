c14d0f86444d20521d5d560144b412fe7a6bfca2
Chawin <ballpor98@gmail.com>
2018-06-12 16:47:58 +0700

    repath image

--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,12 @@ var router = express.Router();
 let obj = require("../api/send.json");
 
 router.post('/', function(req, res, next) {
-  //console.log(req.body.queryResult.parameters);
-  //var para = req.body.queryResult.parameters;
-  //console.log(req.body.queryResult.parameters['shop-name']);
+  //parameters
+  //req.body.queryResult.parameters['shop-name']
   let couponCode = uuidv4();
-  coupon[couponCode] = req.body.queryResult.parameters['shop-name'];
-  //console.log(coupon[couponCode]);
-  obj.fulfillmentMessages[0].text.text[0] = String(coupon[couponCode]+" coupon Code: "+couponCode);
-  //obj.fulfillmentMessages[1].image.imageUri = "https://kouponcoin.herokuapp.com/api/"+couponCode;
-  //+req.body.parameters['shop-name']
-  obj.fulfillmentMessages[1].image.imageUri = "http://chart.googleapis.com/chart?cht=qr&chs=240x240&choe=UTF-8&chld=L|0&chl=8241bd2c-a336-4ffb-8cd2-d9b90bd10467";
+  coupon[couponCode] = true;
+  obj.fulfillmentMessages[0].text.text[0] = String("coupon Code: "+couponCode);
+  obj.fulfillmentMessages[1].image.imageUri = "https://kouponcoin.herokuapp.com/api/"+couponCode;
   res.json(obj);
 });
 router.get('/:id', function(req, res) {
