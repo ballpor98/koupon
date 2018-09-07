a4b3b27441bb97c4086ac83653045986d4e6f753
Chawin <chawin.k@ku.th>
2018-09-07 15:54:09 +0700

    debug

--- routes/echo.js
+++ routes/echo.js
@@ -1,20 +1,8 @@
-'use strict';
+var express = require('express');
+var router = express.Router();
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _express = require('express');
-
-var _express2 = _interopRequireDefault(_express);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var router = _express2.default.Router();
-
-router.post('/', function (req, res) {
+router.post('/', function(req, res, next) {
   console.log(req.body);
-  try {
     var data = {
       "fulfillmentText": "response",
       "fulfillmentMessages": [{
@@ -24,8 +12,6 @@ router.post('/', function (req, res) {
       }]
     };
     res.json({ data: data });
-  } catch (err) {
-    console.log(err);
-  }
 });
-exports.default = router;
+
+module.exports = router;
